b74e12499b1edcbb3e019b270d748b6bf0519767
AgataCli <106405408+AgataCli@users.noreply.github.com>
2022-12-14 17:04:26 -0400

    Prova 1 WEB.
    Primeira prova de Programação Web.

---
+++ prova01/questao_05.js
@@ -0,0 +1,42 @@
+class Venda{
+    constructor(id, quantidade, preco){
+        this.id = id;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    setId(id){
+        this.id = id;
+    }
+
+    getQuantidade(){
+        return this.quantidade;
+    }
+
+    setQuantidade(qtd){
+        this.quantidade = qtd;
+    }
+
+    getPreco(){
+        return this.preco;
+    }
+
+    setPreco(pc){
+        this.preco = pc;
+    }
+
+    getValorTotal(){
+        let valor = this.quantidade * this.preco;
+        return valor;
+    }
+}
+
+let flores = new Venda(2900, 2, 2);
+let undertale = new Venda(1111, 20, 30);
+
+console.log(flores.getValorTotal());
+console.log(undertale.getValorTotal());
